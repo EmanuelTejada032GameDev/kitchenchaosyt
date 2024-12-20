46bb2f55cd4b2f463c07d94b4d362252e3833311
EmanuelTejada032GameDev <emanueltejada311295@gmail.com>
2024-12-19 22:49:03 -0400

    Player script and visual
    add player visual prefab
    Add player.cs to controll player with legacy input syustem awsd key capture and vector trnasformation
    rotation useing transform.forward with slerp

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+
+    void Update()
+    {
+        Vector2 inputVector = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y =  +1;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0 , inputVector.y);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        float rotationSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+
+    }
+}
