7b674ede4366bb55c821309b479825a1f72e6b88
EmanuelTejada032GameDev <emanueltejada311295@gmail.com>
2024-12-23 10:32:16 -0400

    Collision Detection
    basic collition detection  on player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,9 +13,36 @@ public class Player : MonoBehaviour
     void Update()
     {
         Vector2 inputVector = gameInputs.GetMovementVectorNormalized();
-
         Vector3 moveDirection = new Vector3(inputVector.x, 0 , inputVector.y);
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        float playerHeigth = 2f;
+        float playerRadius = .65f;
+        float movementDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeigth, playerRadius, moveDirection, movementDistance);
+
+        if (!canMove)
+        {
+            Vector3 moveDirectionX = new Vector3(inputVector.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeigth, playerRadius, moveDirectionX, movementDistance);
+
+            if (canMove)
+            {
+                moveDirection = moveDirectionX.normalized;
+            }
+            else
+            {
+                Vector3 moveDirectionZ = new Vector3(0, 0, inputVector.y);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeigth, playerRadius, moveDirectionZ, movementDistance);
+                moveDirection = moveDirectionZ.normalized;
+
+            }
+        }
+
+
+        if (canMove)
+        {
+            transform.position += moveDirection * movementDistance;
+        }
 
         isWalking = moveDirection != Vector3.zero;
 
